e09339e507d15f98bf6a72ad785e4efff8538e38
Ricardo Manzano <ricardoamsrams@gmail.com>
2023-08-29 16:15:37 -0400

    Change train function

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 from unet import UNet
 import numpy as np
-
+from preprocessf import CustomDataset
 torch.cuda.empty_cache()
 
 # # Create a U-Net model
@@ -15,64 +15,6 @@ torch.cuda.empty_cache()
 # out_channels = 1  # Single channel mask
 # model = UNet(in_channels, out_channels)
 
-class CustomDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = root_dir
-        self.transform = transform
-        self.image_files = [f for f in os.listdir(os.path.join(root_dir, "images")) if f.endswith(".jpg")]
-    
-    def __len__(self):
-        return len(self.image_files)
-    
-    def __getitem__(self, idx):
-        image_name = self.image_files[idx]
-        image_path = os.path.join(self.root_dir, "images", image_name)
-        mask_path = os.path.join(self.root_dir, "masks", image_name.replace(".jpg", "_mask.gif"))  # Example mask naming
-        
-        image = Image.open(image_path)
-        # image= np.asarray(image)
-        mask = Image.open(mask_path)
-        # mask= np.asarray(image)
-        @staticmethod
-        def preprocess_mask(pil_img,scale):
-            mask_values=[0,1]
-            w, h = pil_img.size
-            newW, newH = int(scale * w), int(scale * h)
-            pil_img = pil_img.resize((newW, newH), resample=Image.NEAREST)
-            img = np.asarray(pil_img)
-
-           
-            mask = np.zeros((newH, newW), dtype=np.int64)
-            for i, v in enumerate(mask_values):
-                if img.ndim == 2:
-                    mask[img == v] = i
-                else:
-                    mask[(img == v).all(-1)] = i
-            return mask
-        
-        @staticmethod
-        def preprocess_image(pil_img,scale):
-            
-            w, h = pil_img.size
-            newW, newH = int(scale * w), int(scale * h)
-            
-            pil_img = pil_img.resize((newW, newH), resample=Image.BICUBIC)
-            img = np.asarray(pil_img)
-
-            if img.ndim == 2:
-                img = img[np.newaxis, ...]
-            else:
-                img = img.transpose((2, 0, 1))
-
-            if (img > 1).any():
-                img = img / 255.0
-            return img
-        scale=0.3
-        image= preprocess_image(image,scale)
-        mask= preprocess_mask(mask,scale)
-               
-
-        return torch.as_tensor(image.copy()).float().contiguous(), torch.as_tensor(mask.copy()).float().contiguous()
     
 def train_model(model):
     batch_size = 2
@@ -90,7 +32,7 @@ def train_model(model):
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     # Training loop
-    num_epochs = 10
+    num_epochs = 3
     for epoch in range(num_epochs):
         model.train()
         print("epoch")
@@ -126,9 +68,10 @@ def train_model(model):
     
 if __name__ == '__main__':
     
-    device = torch.device("cpu" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = UNet(n_channels=3, n_classes=1, bilinear=True)  #n_classes is one because we are trying to detect segmentation.
     model.to(device) 
+    print(model)
     #try:
     model=train_model(model)
    
